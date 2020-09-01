247243276850c00531a182d0b0b703f89834aaba
JoaoCarlosLemos <43700276+JoaoCarlosLemos@users.noreply.github.com>
2020-09-01 17:00:28 -0300

    Update Exercicios _procedure_sql.sql

--- Exercicios _procedure_sql.sql
+++ Exercicios _procedure_sql.sql
@@ -1,11 +1,11 @@
--- Exercícios da Terceira Aula LAB. DE BANCO DE DADOS - (Procedure em SQL-Server) 31-08-2020
+-- ExercÃ­cios da Terceira Aula LAB. DE BANCO DE DADOS - (Procedure em SQL-Server) 31-08-2020
 
 -------------------------------------------------------------------------------------------------------
----------------------------------------------Exercício 01----------------------------------------------
+---------------------------------------------ExercÃ­cio 01----------------------------------------------
 
 -- Criar um database, fazer uma tabela cadastro (cpf, nome, rua, numero e cep)
--- com uma procedure que só permitirá a inserção dos dados se o CPF for válido, 
--- caso contrário retornar uma mensagem de erro
+-- com uma procedure que sÃ³ permitirÃ¡ a inserÃ§Ã£o dos dados se o CPF for vÃ¡lido, 
+-- caso contrÃ¡rio retornar uma mensagem de erro
 
 -------------------------------------------------------------------------------------------------------
 -------------------------------------------------------------------------------------------------------
@@ -20,25 +20,25 @@ GO
 USE ExercicioProcedureCpf
 
 CREATE TABLE cadastro (
-cpf		            VARCHAR(11)		NOT NULL,
-nome		        VARCHAR(70)	    NOT NULL,
+cpf		    VARCHAR(11)		NOT NULL,
+nome		    VARCHAR(70)	        NOT NULL,
 rua  	            VARCHAR(70)		NOT NULL,
-numero  	        INT     		NOT NULL,
+numero  	    INT     		NOT NULL,
 cep  	            VARCHAR(8)		NOT NULL
 PRIMARY KEY (cpf))                       
 
---------------------------------------Procedure Validação de CPF---------------------------------------
+--------------------------------------Procedure ValidaÃ§Ã£o de CPF---------------------------------------
 -------------------------------------------------------------------------------------------------------
 
 CREATE PROCEDURE sp_validacao_cpf (@cpf VARCHAR(11),@resultado VARCHAR(8) OUTPUT)
 AS
 	DECLARE   @indice    INT,
-			  @soma      INT,
-			  @digito1   INT,
-			  @digito2   INT
+	          @soma      INT,
+	          @digito1   INT,
+	          @digito2   INT
           
 
-	--Cálculo do 1º dígito
+	--CÃ¡lculo do 1Âº dÃ­gito
 
 	SET @soma = 0
 	SET @indice = 1
@@ -56,7 +56,7 @@ AS
 		SET @digito1 = 0;
 	END
 
-	-- Cálculo do 2º dígito 
+	-- CÃ¡lculo do 2Âº dÃ­gito 
 
 	SET @soma = 0
 	SET @indice = 1
@@ -74,7 +74,7 @@ AS
 		SET @digito2 = 0;
 	END
 
-	-- Validacão
+	-- ValidacÃ£o
 
 	IF (@digito1 = SUBSTRING(@cpf,LEN(@cpf)-1,1)) AND (@digito2 = SUBSTRING(@cpf,LEN(@cpf),1))
 	BEGIN
@@ -92,7 +92,7 @@ AS
 -------------------------------------------------------------------------------------------------------
 
 CREATE PROCEDURE sp_inset_cadastro (@cpf VARCHAR(11),@nome VARCHAR(70),@rua VARCHAR(70),@numero INT,
-									@cep VARCHAR(8))
+				    @cep VARCHAR(8))
 AS
 	DECLARE @out VARCHAR(8)
 
@@ -108,7 +108,7 @@ AS
 	BEGIN
 		IF(@out = 'INVALIDO')
 		BEGIN
-			RAISERROR('CPF INVÁLIDO, NÃO FOI POSSÍVEL INSERIR O REGISTRO', 16, 1)
+			RAISERROR('CPF INVÃLIDO, NÃƒO FOI POSSÃVEL INSERIR O REGISTRO', 16, 1)
 		END
 	END
 
@@ -116,10 +116,10 @@ AS
 ----------------------------------Chamada da procedure insert_cadastro---------------------------------
 -------------------------------------------------------------------------------------------------------
 
--- CPF Válido
-EXEC sp_inset_cadastro '22233366638','João Carlos Lemos','Flor de Indio',138,'08061230'
+-- CPF VÃ¡lido
+EXEC sp_inset_cadastro '22233366638','JoÃ£o Carlos Lemos','Flor de Indio',138,'08061230'
 
--- CPF Inválido
+-- CPF InvÃ¡lido
 EXEC sp_inset_cadastro '22233366639','Maria da Silva','Flor de Noiva',764,'08061265'
 
 
@@ -127,7 +127,7 @@ SELECT * FROM cadastro
 
 
 -------------------------------------------------------------------------------------------------------
----------------------------------------------Exercício 02----------------------------------------------
+---------------------------------------------ExercÃ­cio 02----------------------------------------------
 
 /*
 Criar uma database chamada academia, com 3 tabelas como seguem:
@@ -136,7 +136,7 @@ Aluno
 |Codigo_aluno|Nome|
 
 Atividade
-|Codigo|Descrição|IMC|
+|Codigo|DescriÃ§Ã£o|IMC|
 
 Atividade
 codigo      descricao                           imc
@@ -150,17 +150,17 @@ codigo      descricao                           imc
 Atividadesaluno
 |Codigo_aluno|Altura|Peso|IMC|Atividade|
 
-IMC = Peso (Kg) / Altura² (M)
+IMC = Peso (Kg) / AlturaÂ² (M)
 
 Atividade: Buscar a PRIMEIRA atividade referente ao IMC imediatamente acima do calculado.
-* Caso o IMC seja maior que 40, utilizar o código 5.
+* Caso o IMC seja maior que 40, utilizar o cÃ³digo 5.
 
 Criar uma Stored Procedure (sp_alunoatividades), com as seguintes regras:
- - Se, dos dados inseridos, o código for nulo, mas, existirem nome, altura, peso, deve-se inserir um 
+ - Se, dos dados inseridos, o cÃ³digo for nulo, mas, existirem nome, altura, peso, deve-se inserir um 
  novo registro nas tabelas aluno e aluno atividade com o imc calculado e as atividades pelas 
  regras estabelecidas acima.
- - Se, dos dados inseridos, o nome for (ou não nulo), mas, existirem código, altura, peso, deve-se 
- verificar se aquele código existe na base de dados e atualizar a altura, o peso, o imc calculado e 
+ - Se, dos dados inseridos, o nome for (ou nÃ£o nulo), mas, existirem cÃ³digo, altura, peso, deve-se 
+ verificar se aquele cÃ³digo existe na base de dados e atualizar a altura, o peso, o imc calculado e 
  as atividades pelas regras estabelecidas acima.
 */
 
@@ -178,30 +178,30 @@ USE Academia
 
 
 CREATE TABLE aluno (
-codigo_aluno  	    INT     		NOT NULL,
-nome		        VARCHAR(70)	    NULL
+codigo_aluno  	    INT     	    NOT NULL,
+nome		    VARCHAR(70)	    NULL
 PRIMARY KEY (Codigo_aluno)) 
 
 
 CREATE TABLE atividade (
-codigo_atividade  	    INT     		NOT NULL,
-descricao		        VARCHAR(70)	    NOT NULL,
+codigo_atividade  	INT             NOT NULL,
+descricao		VARCHAR(70)	NOT NULL,
 imc                     DECIMAL(7,1)	NULL
 PRIMARY KEY (Codigo_atividade))
 
 
 CREATE TABLE Atividadesaluno (
-codigo_aluno		INT				NOT NULL,
-altura				DECIMAL(7,2)	NULL,
-peso				DECIMAL(7,2)    NULL,
-imc					DECIMAL(7,1)    NULL,
-codigo_atividade  	INT			    NOT NULL
+codigo_aluno		INT		NOT NULL,
+altura			DECIMAL(7,2)	NULL,
+peso			DECIMAL(7,2)    NULL,
+imc			DECIMAL(7,1)    NULL,
+codigo_atividade  	INT		NOT NULL
 PRIMARY KEY (codigo_aluno,codigo_atividade),
 FOREIGN KEY (codigo_aluno) REFERENCES aluno (codigo_aluno),
 FOREIGN KEY (codigo_atividade) REFERENCES atividade (codigo_atividade))
 
 
------------------------------------------Inserção de Regidtros-----------------------------------------
+-----------------------------------------InserÃ§Ã£o de Regidtros-----------------------------------------
 -------------------------------------------------------------------------------------------------------
 
 INSERT INTO atividade VALUES 
@@ -212,7 +212,7 @@ INSERT INTO atividade VALUES
 (5, 'Esteira + Bicicleta',39.9)
 
 INSERT INTO aluno VALUES 
-(1, 'João Carlos'),
+(1, 'JoÃ£o Carlos'),
 (2,'Ana Maria')
 
 --------------------------------------Procedure do Calculo do IMC--------------------------------------
@@ -224,15 +224,15 @@ AS
 	SET @imc= @peso/(POWER(@altura,2))
 
 
-----------------------------Procedure de inserção na tabela atividadeAluno-----------------------------
+----------------------------Procedure de inserÃ§Ã£o na tabela atividadeAluno-----------------------------
 -------------------------------------------------------------------------------------------------------
 
 
 CREATE PROCEDURE sp_alunoatividades (@codigo_aluno INT,@nome VARCHAR(70),@altura DECIMAL(7,2),@peso DECIMAL(7,2))
 AS	
-	DECLARE @imc								DECIMAL(7,1),    
-			@codigo_atividade		 			INT,
-			@verificacao_codigo_aluno			INT
+	DECLARE @imc				DECIMAL(7,1),    
+		@codigo_atividade		INT,
+		@verificacao_codigo_aluno	INT
 		
 
 
@@ -244,10 +244,10 @@ AS
 	END
 	ELSE
 	BEGIN
-		--verificar se aquele código existe na tabela aluno
+		--verificar se aquele cÃ³digo existe na tabela aluno
 		SET @verificacao_codigo_aluno= (SELECT COUNT(codigo_aluno) FROM aluno WHERE codigo_aluno=@codigo_aluno)
 
-		--se não existir, fazer INSERT na tabela aluno
+		--se nÃ£o existir, fazer INSERT na tabela aluno
 		IF(@verificacao_codigo_aluno=0)
 		BEGIN
 			INSERT INTO aluno VALUES (@codigo_aluno,@nome)
@@ -262,7 +262,7 @@ AS
 	IF(@nome IS NOT NULL OR @nome IS NULL AND @codigo_aluno IS NOT NULL AND @altura IS NOT NULL AND @peso IS NOT NULL)
 	BEGIN
 
-		--verificar se aquele código existe na base de dados
+		--verificar se aquele cÃ³digo existe na base de dados
 		SET @verificacao_codigo_aluno= (SELECT COUNT(codigo_aluno) FROM Atividadesaluno WHERE codigo_aluno=@codigo_aluno)
 
 		-- se existir o codigo, fazer UPDATE
@@ -307,7 +307,7 @@ AS
 			END
 		END
 
-		-- se não existir o codigo, fazer INSERT
+		-- se nÃ£o existir o codigo, fazer INSERT
 		ELSE
 		BEGIN
 			IF(@imc>=18.5 AND @imc<24.9)
@@ -358,7 +358,7 @@ CREATE VIEW v_atividades_alunos
 AS
 
 SELECT Atividadesaluno.codigo_aluno,aluno.nome,Atividadesaluno.altura,Atividadesaluno.peso,Atividadesaluno.imc,
-       Atividadesaluno.codigo_atividade,atividade.descricao AS 'descrição_atividade'FROM atividadesaluno
+       Atividadesaluno.codigo_atividade,atividade.descricao AS 'descriÃ§Ã£o_atividade'FROM atividadesaluno
 INNER JOIN aluno     ON  aluno.codigo_aluno         = Atividadesaluno.codigo_aluno
 INNER JOIN atividade ON  atividade.codigo_atividade = Atividadesaluno.codigo_atividade
 
@@ -366,12 +366,12 @@ INNER JOIN atividade ON  atividade.codigo_atividade = Atividadesaluno.codigo_ati
 -------------------------------Chamada da Procedure (sp_alunoatividades)-------------------------------
 -------------------------------------------------------------------------------------------------------
 
---Parâmetros = codigo_aluno INT, nome VARCHAR(70), altura DECIMSAL(7,2), peso DECIMAL(7,2)
+--ParÃ¢metros = codigo_aluno INT, nome VARCHAR(70), altura DECIMSAL(7,2), peso DECIMAL(7,2)
 
 --codigo existente na tabela aluno
-EXEC sp_alunoatividades 1,'João Carlos',1.80,129
+EXEC sp_alunoatividades 1,'JoÃ£o Carlos',1.80,129
 
---codigo não existente na tabela aluno
+--codigo nÃ£o existente na tabela aluno
 EXEC sp_alunoatividades 5,'mariana',1.60,71
 
 ----------------------------------Chamada View (v_atividades_alunos)-----------------------------------
